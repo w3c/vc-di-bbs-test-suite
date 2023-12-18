d81e29db5c351431fa55c3d32d6e6a45432eb4ce
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-12-18 12:23:17 -0500

    Remove ECDSA-related assertion helpers.

--- tests/assertions.js
+++ tests/assertions.js
@@ -1,12 +1,7 @@
 /*!
  * Copyright 2023 Digital Bazaar, Inc. All Rights Reserved
  */
-import {
-  multibaseMultikeyHeaderP256, multibaseMultikeyHeaderP384,
-} from './helpers.js';
 import chai from 'chai';
-import {decode} from 'base58-universal';
-import varint from 'varint';
 
 const should = chai.should();
 
@@ -15,42 +10,6 @@ const bs58 =
   /^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]+$/;
 // assert something is entirely bs58 encoded
 export const shouldBeBs58 = s => bs58.test(s);
-
-export const shouldBeMulticodecEncoded = async s => {
-  // check if it is multi codec encoded
-  if(s.startsWith(multibaseMultikeyHeaderP256)) {
-    // example of a P-256 publicKeyMultibase -
-    // zDnaepHgv4AU1btQ8dp6EYbvgJ6M1ovzKnSpXJUPU2hshXLvp
-    const bytes = await decode(s.slice(1));
-    bytes.length.should.equal(35);
-    // bytes example => Uint8Array(35) [
-    //   128,  36,   3,  98, 121, 153, 205, 199,
-    //    39, 148, 212,  49, 157,  57, 152, 184,
-    //    97,  14, 217, 198,  76,  50,  46, 169,
-    //    58, 124, 244, 202, 141, 161,  92,  55,
-    //   122, 233, 205
-    // ]
-    // get the two-byte prefix
-    const prefix = Array.from(bytes.slice(0, 2));
-    // the multicodec encoding of a P-256 public key is the two-byte
-    // prefix 0x1200 followed by the 33-byte compressed public key data.
-    const expectedP256Prefix = await varint.encode(0x1200);
-    return JSON.stringify(prefix) === JSON.stringify(expectedP256Prefix);
-  }
-
-  if(s.startsWith(multibaseMultikeyHeaderP384)) {
-    const bytes = await decode(s.slice(1));
-    bytes.length.should.equal(51);
-    // get the two-byte prefix
-    const prefix = Array.from(bytes.slice(0, 2));
-    // the multicodec encoding of a P-384 public key is the two-byte prefix
-    // 0x1201 followed by the 49-byte compressed public key data.
-    const expectedP384Prefix = await varint.encode(0x1201);
-    return JSON.stringify(prefix) === JSON.stringify(expectedP384Prefix);
-  }
-  // Unsupported key type, return false
-  return false;
-};
 
 export const verificationFail = async ({credential, verifier}) => {
   const body = {
