3e701a5ffaef71e61bd348656329842bda496d09
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-12-19 09:42:01 -0500

    Update interop tests and fix subtitle in `respecConfig.json`.

--- tests/30-bbs-interop.js
+++ tests/30-bbs-interop.js
@@ -1,82 +1,55 @@
-// /*!
-//  * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
-//  */
-// import {createDisclosedVc, createInitialVc} from './helpers.js';
-// import {endpoints} from 'vc-test-suite-implementations';
-// import {validVc as vc} from './mock-data.js';
-// import {verificationSuccess} from './assertions.js';
+/*!
+ * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
+ */
+import {createDisclosedVc, createInitialVc} from './helpers.js';
+import {endpoints} from 'vc-test-suite-implementations';
+import {validVc as vc} from './mock-data.js';
+import {verificationSuccess} from './assertions.js';
 
-// const tag = 'bbs-2023';
+const tag = 'bbs-2023';
 
-// // only use implementations with `bbs-2023` issuers.
-// const {
-//   match: issuerMatches
-// } = endpoints.filterByTag({tags: [tag], property: 'issuers'});
-// const {
-//   match: verifierMatches
-// } = endpoints.filterByTag({tags: [tag], property: 'verifiers'});
+// only use implementations with `bbs-2023` issuers.
+const {
+  match: issuerMatches
+} = endpoints.filterByTag({tags: [tag], property: 'issuers'});
+const {
+  match: verifierMatches
+} = endpoints.filterByTag({tags: [tag], property: 'verifiers'});
 
-// describe('bbs-2023 (interop)', function() {
-//   // this will tell the report
-//   // to make an interop matrix with this suite
-//   this.matrix = true;
-//   this.report = true;
-//   this.implemented = [];
-//   this.rowLabel = 'Issuer';
-//   this.columnLabel = 'Verifier';
-
-//   const issuers = [];
-//   for(const [issuerName, {endpoints}] of issuerMatches) {
-//     for(const issuerEndpoint of endpoints) {
-//       issuers.push({
-//         issuerName,
-//         issuerEndpoint
-//       });
-//     }
-//   }
-
-//   const verifiers = [];
-//   for(const [verifierName, {endpoints}] of verifierMatches) {
-//     for(const verifierEndpoint of endpoints) {
-//       verifiers.push({
-//         verifierName,
-//         verifierEndpoint
-//       });
-//       // add verifiers' names for reporting
-//       this.implemented.push(verifierName);
-//     }
-//   }
-
-//   for(const {
-//     issuerDisplayName, issuerEndpoint
-//   } of issuers) {
-//     for(const {verifierDisplayName, verifierEndpoint} of verifiers) {
-//       let disclosedCredential;
-//       before(async function() {
-//         const issuedVc = await createInitialVc({issuer: issuerEndpoint, vc});
-//         const {match: matchingVcHolders} = endpoints.filterByTag({
-//           tags: ['vcHolder'],
-//           property: 'vcHolders'
-//         });
-//         // Use DB vc holder to create disclosed credentials
-//         const vcHolders = matchingVcHolders.get('Digital Bazaar').endpoints;
-//         const vcHolder = vcHolders[0];
-//         ({disclosedCredential} = await createDisclosedVc({
-//           selectivePointers: ['/credentialSubject/id'],
-//           signedCredential: issuedVc,
-//           vcHolder
-//         }));
-//       });
-//       it(`"${verifierDisplayName}" should verify "${issuerDisplayName}"`,
-//         async function() {
-//           this.test.cell = {
-//             rowId: issuerDisplayName,
-//             columnId: verifierDisplayName
-//           };
-//           await verificationSuccess({
-//             credential: disclosedCredential, verifier: verifierEndpoint
-//           });
-//         });
-//     }
-//   }
-// });
+describe('bbs-2023 (interop)', function() {
+  // this will tell the report
+  // to make an interop matrix with this suite
+  this.matrix = true;
+  this.report = true;
+  this.implemented = [...verifierMatches.keys()];
+  this.rowLabel = 'Issuer';
+  this.columnLabel = 'Verifier';
+  for(const [issuerName, {endpoints: issuerEndpoints}] of issuerMatches) {
+    let disclosedCredential;
+    before(async function() {
+      const [issuer] = issuerEndpoints;
+      const issuedVc = await createInitialVc({issuer, vc});
+      const {match: matchingVcHolders} = endpoints.filterByTag({
+        tags: ['vcHolder'],
+        property: 'vcHolders'
+      });
+      // Use DB vc holder to create disclosed credentials
+      const vcHolders = matchingVcHolders.get('Digital Bazaar').endpoints;
+      const vcHolder = vcHolders[0];
+      ({disclosedCredential} = await createDisclosedVc({
+        selectivePointers: ['/credentialSubject/id'],
+        signedCredential: issuedVc,
+        vcHolder
+      }));
+    });
+    for(const [verifierName, {endpoints}] of verifierMatches) {
+      const [verifier] = endpoints;
+      it(`${verifierName} should verify ${issuerName}`, async function() {
+        this.test.cell = {rowId: issuerName, columnId: verifierName};
+        await verificationSuccess({
+          credential: disclosedCredential, verifier
+        });
+      });
+    }
+  }
+});
