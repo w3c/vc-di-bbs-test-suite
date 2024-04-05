a334714b42fa560f987abe21b73893a149b9ca69
Andrew Jones <ajones@digitalbazaar.com>
2024-04-05 18:28:36 +0000

    Add support for bls12-381-multikey.

--- tests/didResolver.js
+++ tests/didResolver.js
@@ -3,10 +3,14 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-
+import * as Bls12381Multikey from '@digitalbazaar/bls12-381-multikey';
 import * as didMethodKey from '@digitalbazaar/did-method-key';
 
 const didKeyDriver = didMethodKey.driver();
+didKeyDriver.use({
+  multibaseMultikeyHeader: 'zUC7',
+  fromMultibase: Bls12381Multikey.from
+});
 
 export async function didResolver({url}) {
   if(url.startsWith('did:')) {
