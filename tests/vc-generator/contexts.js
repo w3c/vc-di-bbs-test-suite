9300ea5c01d44825b766e725872b9c19e234eb54
Andrew Jones <ajones@digitalbazaar.com>
2024-04-18 19:28:53 +0000

    Add contexts to vc-gen.

---
+++ tests/vc-generator/contexts.js
@@ -0,0 +1,45 @@
+/*!
+ * Copyright (c) 2022-2024 Digital Bazaar, Inc.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+import credentialsCtx from 'credentials-context';
+import credentialsV2Ctx from 'credentials-v2-context';
+import dataIntegrityCtx from '@digitalbazaar/data-integrity-context';
+import didCtx from '@digitalcredentials/did-context';
+import {klona} from 'klona';
+import multikeyCtx from '@digitalbazaar/multikey-context';
+
+const contextMap = new Map();
+
+const _dataIntegrityCtx = klona(dataIntegrityCtx.CONTEXT);
+const diCtx = _dataIntegrityCtx['@context'];
+// add UnknownProofType to local context for test data
+diCtx.UnknownProofType =
+  klona(_dataIntegrityCtx['@context'].DataIntegrityProof);
+// add invalidPurpose to context for test data
+diCtx.DataIntegrityProof['@context'].proofPurpose['@context'].invalidPurpose = {
+  '@id': 'https://w3id.org/security#invalidPurpose',
+  '@type': '@id',
+  '@container': '@set'
+};
+
+// add contexts for the documentLoader
+contextMap.set(multikeyCtx.constants.CONTEXT_URL, multikeyCtx.CONTEXT);
+contextMap.set(
+  dataIntegrityCtx.constants.CONTEXT_URL,
+  _dataIntegrityCtx
+);
+
+const setContexts = contexts => {
+  for(const [key, value] of contexts) {
+    contextMap.set(key, value);
+  }
+};
+// add the dids contexts
+setContexts(didCtx.contexts);
+// add the credentials v1 contexts
+setContexts(credentialsCtx.contexts);
+// add the credentials v2 contexts
+setContexts(credentialsV2Ctx.contexts);
+
+export {contextMap};
