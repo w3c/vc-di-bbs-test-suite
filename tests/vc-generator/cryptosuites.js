bb5ddbc1997fc9dc08c71ee3dad7f2fe1425c278
Andrew Jones <ajones@digitalbazaar.com>
2024-04-18 19:40:29 +0000

    Add index and cryptosuites to vc-gen.

---
+++ tests/vc-generator/cryptosuites.js
@@ -0,0 +1,26 @@
+/*!
+ * Copyright 2023 Digital Bazaar, Inc.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+import * as bbs2023Cryptosuite from '../lib/index.js';
+
+export const getSuite = ({suite, mandatoryPointers, selectivePointers}) => {
+  switch(suite) {
+    case `bbs-2023`: {
+      if(mandatoryPointers) {
+        return bbs2023Cryptosuite.createSignCryptosuite({
+          mandatoryPointers
+        });
+      }
+      if(selectivePointers) {
+        return bbs2023Cryptosuite.createDiscloseCryptosuite({
+          selectivePointers
+        });
+      }
+      throw new Error('Suite "ecdsa-sd-2023" requires either mandatory or ' +
+        'selective pointers');
+    }
+    default:
+      throw new Error(`Unsupported cryptosuite suite: ${suite}`);
+  }
+};
