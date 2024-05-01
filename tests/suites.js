d58c9ad86d50a1ac476c46f47cac4b592a088a79
Andrew Jones <ajones@digitalbazaar.com>
2024-05-01 19:08:01 +0000

    Update tests/10-bbs-create.js use chrome highlight link.
    Co-authored-by: BigBlueHat <byoung@digitalbazaar.com>

--- tests/suites.js
+++ tests/suites.js
@@ -72,7 +72,7 @@ export function createSuite({
         });
         /*
         * Checked on 04-15-2024.
-        * {@link https://w3c.github.io/vc-di-bbs/#dataintegrityproof}
+        * @link https://w3c.github.io/vc-di-bbs/#dataintegrityproof:~:text=The%20value%20of%20the%20proofValue%20property%20of%20the%20proof%20MUST%20be%20a%20BBS%20signature%20or%20BBS%20proof%20produced%20according%20to%20%5BCFRG%2DBBS%2DSIGNATURE%5D%20that%20is%20serialized%20and%20encoded%20according%20to%20procedures%20in%20section%203.%20Algorithms.
         * Link to relevant section above.
         */
         it('The value of the proofValue property of the proof MUST be a BBS ' +
