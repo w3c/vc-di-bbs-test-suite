740d19ddafe2caa5ea2a1842f81668dd2db1d859
Andrew Jones <ajones@digitalbazaar.com>
2024-05-06 13:45:57 +0000

    Vc Gen getSuite now returns DataIntegrityProof.

--- tests/vc-generator/cryptosuites.js
+++ tests/vc-generator/cryptosuites.js
@@ -1,20 +1,38 @@
 /*!
- * Copyright 2023 Digital Bazaar, Inc.
+ * Copyright 2023-2024 Digital Bazaar, Inc.
  * SPDX-License-Identifier: BSD-3-Clause
  */
 import * as bbs2023Cryptosuite from '@digitalbazaar/bbs-2023-cryptosuite';
+import {DataIntegrityProof} from '@digitalbazaar/data-integrity';
 
-export const getSuite = ({suite, mandatoryPointers, selectivePointers}) => {
+export const getSuite = ({
+  suite,
+  signer,
+  mandatoryPointers,
+  selectivePointers,
+  verify
+}) => {
   switch(suite) {
     case `bbs-2023`: {
       if(mandatoryPointers) {
-        return bbs2023Cryptosuite.createSignCryptosuite({
-          mandatoryPointers
+        return new DataIntegrityProof({
+          signer,
+          cryptosuite: bbs2023Cryptosuite.createSignCryptosuite({
+            mandatoryPointers
+          })
         });
       }
       if(selectivePointers) {
-        return bbs2023Cryptosuite.createDiscloseCryptosuite({
-          selectivePointers
+        return new DataIntegrityProof({
+          signer,
+          cryptosuite: bbs2023Cryptosuite.createDiscloseCryptosuite({
+            selectivePointers
+          })
+        });
+      }
+      if(verify) {
+        return new DataIntegrityProof({
+          cryptosuite: bbs2023Cryptosuite.createVerifyCryptosuite()
         });
       }
       throw new Error('Suite "ecdsa-sd-2023" requires either mandatory or ' +
