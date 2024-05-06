3de7697d7ea37af65e1a1f66bcfb92fb4b29e4a0
Andrew Jones <ajones@digitalbazaar.com>
2024-05-06 19:55:29 +0000

    Add did:key with Bls12381 multikey support to documentLoader.

--- tests/vc-generator/documentLoader.js
+++ tests/vc-generator/documentLoader.js
@@ -2,8 +2,11 @@
  * Copyright (c) 2022-2024 Digital Bazaar, Inc.
  * SPDX-License-Identifier: BSD-3-Clause
  */
+import * as Bls12381Multikey from '@digitalbazaar/bls12-381-multikey';
+import * as didKey from '@digitalbazaar/did-method-key';
 import {contextMap} from './contexts.js';
 import {JsonLdDocumentLoader} from 'jsonld-document-loader';
+
 const jdl = new JsonLdDocumentLoader();
 
 // add contexts to documentLoad
@@ -11,6 +14,31 @@ for(const [key, value] of contextMap) {
   jdl.addStatic(key, value);
 }
 
-const documentLoader = jdl.build();
+const _documentLoader = jdl.build();
+
+const driver = didKey.driver();
+
+const keyTypes = [
+  {keyType: 'P-381', header: 'zUC7', fromMultibase: Bls12381Multikey.from}
+];
+
+for(const {header, fromMultibase} of keyTypes) {
+  driver.use({
+    multibaseMultikeyHeader: header,
+    fromMultibase
+  });
+}
+
+const documentLoader = async url => {
+  if(url.startsWith('did:')) {
+    const document = await driver.get({did: url});
+    return {
+      contextUrl: null,
+      documentUrl: url,
+      document
+    };
+  }
+  return _documentLoader(url);
+};
 
 export {documentLoader};
