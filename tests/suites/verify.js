1f5c13748c02724dbc8bad071afcb928a212be85
Andrew Jones <ajones@digitalbazaar.com>
2024-05-07 16:27:00 +0000

    Add checked and link to test.

--- tests/suites/verify.js
+++ tests/suites/verify.js
@@ -103,6 +103,10 @@ export function verifySuite({
               credential: signedCredentialCopy, verifier
             });
           });
+        /*
+         * Checked on 2024-04-16
+         * @{link https://w3c.github.io/vc-di-bbs/#create-base-proof-bbs-2023:~:text=The%20transformation%20options%20MUST%20contain%20a%20type%20identifier%20for%20the%20cryptographic%20suite%20(type)%2C%20a%20cryptosuite%20identifier%20(cryptosuite)%2C%20and%20a%20verification%20method%20(verificationMethod).}
+         */
         it('The transformation options MUST contain a type identifier for ' +
         'the cryptographic suite (type), a cryptosuite identifier ' +
         '(cryptosuite), and a verification method (verificationMethod).',
