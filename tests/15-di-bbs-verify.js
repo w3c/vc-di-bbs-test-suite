da23459367856376467f5804d0f18811b84a82f6
Andrew Jones <ajones@digitalbazaar.com>
2024-06-04 20:50:36 +0000

    Factor out getMultiKey & use in newer DI verifier tests.

--- tests/15-di-bbs-verify.js
+++ tests/15-di-bbs-verify.js
@@ -3,11 +3,12 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-
+import * as bbs2023Cryptosuite from '@digitalbazaar/bbs-2023-cryptosuite';
 import {
   checkDataIntegrityProofVerifyErrors
 } from 'data-integrity-test-suite-assertion';
 import {endpoints} from 'vc-test-suite-implementations';
+import {getMultiKey} from './vc-generator/key-gen.js';
 
 const tag = 'bbs-2023';
 // only use implementations with `bbs-2023` verifiers.
@@ -15,7 +16,16 @@ const {match} = endpoints.filterByTag({
   tags: [tag],
   property: 'verifiers'
 });
+const testDataOptions = {
+  suiteName: 'bbs-2023',
+  cryptosuite: bbs2023Cryptosuite,
+  mandatoryPointers: ['/issuer'],
+  selectivePointers: ['/credentialSubject'],
+  key: await getMultiKey({keyType: 'P-381'})
+};
+
 checkDataIntegrityProofVerifyErrors({
   implemented: match,
-  testDescription: 'Data Integrity (bbs-2023 verifiers)'
+  testDescription: 'Data Integrity (bbs-2023 verifiers)',
+  testDataOptions
 });
