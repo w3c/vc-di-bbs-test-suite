c91ad145e39a41430c4deb240ee713bee64ab252
Andrew Jones <ajones@digitalbazaar.com>
2024-06-21 15:00:38 +0000

    Add proxyStub method, add createProofValue mock, use createProofValue mock.

--- tests/vc-generator/generators.js
+++ tests/vc-generator/generators.js
@@ -7,22 +7,40 @@ import * as mocks from './mockMethods.js';
 export function noProofTypeorCryptosuite({
   suite,
   selectiveSuite,
-  proofType,
-  cryptosuiteName,
   ...args}) {
-
-  return {...args, suite, selectiveSuite, proofType, cryptosuiteName};
+  suite._cryptosuite = _proxyStub({
+    object: suite._cryptosuite,
+    mocks: {createVerifyData: mocks.createVerifyData},
+  });
+  return {...args, suite, selectiveSuite};
 }
 
 export function invalidStringEncoding({suite, selectiveSuite, ...args}) {
-  suite._cryptosuite = new Proxy(suite._cryptosuite, {
+  suite._cryptosuite = _proxyStub({
+    object: suite._cryptosuite,
+    mocks: {createProofValue: mocks.createProofValue}
+  });
+  return {...args, suite, selectiveSuite};
+}
+
+/**
+ * Creates a Proxy around a method on an object.
+ *
+ * @param {object} options - Options to use.
+ * @param {object} options.object - The object to proxy.
+ * @param {{[key: string]: Function}} options.mocks - A collection of mock
+ *   methods.
+ *
+ * @returns {Proxy} - Returns a Proxy.
+ */
+function _proxyStub({object, mocks}) {
+  return new Proxy(object, {
     get(target, prop) {
-      if(prop === 'createProofValue') {
-        return mocks.createProofValue;
+      const mock = mocks[prop];
+      if(mock) {
+        return mock;
       }
-      // if a stub is not found, return the original property
       return Reflect.get(...arguments);
     }
   });
-  return {...args, suite, selectiveSuite};
 }
