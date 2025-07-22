79a04710fcbcc36ac5e3e04bbf5316b561b7f05e
ashushukla0412 <ashushukla04121998@gmail.com>
2025-07-22 11:46:43 +0530

    Use local document loader with vdlCtx
    Signed-off-by: ashushukla0412 <ashushukla04121998@gmail.com>

--- tests/vc-generator/contexts.js
+++ tests/vc-generator/contexts.js
@@ -14,27 +14,42 @@ import dataIntegrityCtx from '@digitalbazaar/data-integrity-context';
 import didCtx from '@digitalcredentials/did-context';
 import multikeyCtx from '@digitalbazaar/multikey-context';
 
-const contextMap = new Map(credentialsContexts);
+const contextMap = new Map();
 const setContexts = contexts => {
   for(const [key, value] of contexts) {
     contextMap.set(key, structuredClone(value));
   }
 };
+const invalidPurpose = {
+  '@id': 'https://w3id.org/security#invalidPurpose',
+  '@type': '@id',
+  '@container': '@set'
+};
 
-const {context: vc2Context} = namedCredentialsContexts.get('v2');
-const v2Ctx = vc2Context['@context'];
+// adding terms manually
+const {
+  id: v2ContextUrl,
+  context: v2Context
+} = structuredClone(namedCredentialsContexts.get('v2'));
+const v2Ctx = v2Context['@context'];
 v2Ctx.UnknownProofType = structuredClone(v2Ctx.DataIntegrityProof);
+v2Ctx.DataIntegrityProof['@context'].proofPurpose['@context'].invalidPurpose =
+  invalidPurpose;
+v2Ctx.undefinedTerm = 'urn:example:undefinedTerm';
+
 const _dataIntegrityCtx = structuredClone(dataIntegrityCtx.CONTEXT);
 const diCtx = _dataIntegrityCtx['@context'];
 diCtx.UnknownProofType = structuredClone(diCtx.DataIntegrityProof);
-contextMap.set(
-  dataIntegrityCtx.constants.CONTEXT_URL,
-  _dataIntegrityCtx
-);
-// add contexts for the documentLoader
+diCtx.DataIntegrityProof['@context'].proofPurpose['@context'].invalidPurpose =
+  invalidPurpose;
+diCtx.undefinedTerm = 'urn:example:undefinedTerm';
+
+// adding to contextMap
+setContexts(didCtx.contexts);
+setContexts(credentialsContexts);
 contextMap.set(multikeyCtx.constants.CONTEXT_URL, multikeyCtx.CONTEXT);
 contextMap.set(vdlCtxUrl, vdlCtx);
-// add the dids contexts
-setContexts(didCtx.contexts);
+contextMap.set(dataIntegrityCtx.constants.CONTEXT_URL, _dataIntegrityCtx);
+contextMap.set(v2ContextUrl, v2Context);
 
 export {contextMap};
